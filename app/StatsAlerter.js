8d6a70c03e6501634c449e660c716a2897ce1294
ShrutiRajpoot <39909922+ShrutiRajpoot@users.noreply.github.com>
2022-10-18 16:07:49 +0530

    add max

--- app/StatsAlerter.js
+++ app/StatsAlerter.js
@@ -3,8 +3,14 @@
           this.maxThreshold = maxThreshold;
           this.alerters = alerters;
       }
+    getMax(arr){
+        let res = arr[0];
+        for(let i = 1; i < arr.length; i++)
+        res = Math.max(res, arr[i]);
+        return res;
+    }
       checkAndAlert(arr) {
-          let max = getMax(arr);
+          let max = this.getMax(arr);
           if(max > this.maxThreshold){
             this.alerters[0].emailSent = true;
             this.alerters[1].ledGlows = true
