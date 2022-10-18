168825fc2c108d616b121b4ce385aed06c17f1a4
ShrutiRajpoot <39909922+ShrutiRajpoot@users.noreply.github.com>
2022-10-18 12:03:42 +0530

    clean

--- app/statistics.js
+++ app/statistics.js
@@ -1,19 +1,19 @@
 module.exports=function getStatistics(numbers){
     
     //implement the computation of statistics here
-//     stats = {
-//     min: '',
-//     max: '',
-//     average:''
-//     };
+    stats = {
+    min: '',
+    max: '',
+    average:''
+    };
     
-//     function getMin(arr)
-//     {
-//         let res = arr[0];
-//         for(let i = 1; i < arr.length; i++)
-//         res = Math.min(res, arr[i]);
-//         return res;
-//     }
+    function getMin(arr)
+    {
+        let res = arr[0];
+        for(let i = 1; i < arr.length; i++)
+        res = Math.min(res, arr[i]);
+        return res;
+    }
     function getMax(arr)
     {
         let res = arr[0];
@@ -21,46 +21,15 @@ module.exports=function getStatistics(numbers){
         res = Math.max(res, arr[i]);
         return res;
     }
-//     function getAverage(arr){
-//         let sum = 0;
-//         for(let i = 0; i < arr.length; i++)
-//          sum = sum + arr[i];
-//         return sum/arr.length;
-//     }
-//     stats.min = getMin(numbers);
-//     stats.max = getMax(numbers);
-//     stats.average = getAverage(numbers);
-    console.log(stats,"**********************************");
-    
-    function EmailAlert(){
-        this.emailSent = false;
+    function getAverage(arr){
+        let sum = 0;
+        for(let i = 0; i < arr.length; i++)
+         sum = sum + arr[i];
+        return sum/arr.length;
     }
-    function LEDAlert(){
-        this.ledGlows = false;
-    }
-    const ledAlert=new LEDAlert();
-    const emailAlert=new EmailAlert();
-    const alerters=[emailAlert,ledAlert];
-    const maxThreshold = 10.2;
-    console.log("ledAlert", ledAlert);
-    console.log("emailAlert", emailAlert);
-    console.log("alerters", alerters);
-    class StatsAlerter {
-      constructor(maxThreshold, alerters) {
-          this.maxThreshold = maxThreshold;
-          this.alerters = alerters;
-      }
-      checkAndAlert(arr) {
-          let max = getMax(arr);
-          if(max > this.maxThreshold){
-            this.alerters[0].emailSent = true;
-            this.alerters[1].ledGlows = true
-          }
-      }
-    }
-    const statsAlerter=new StatsAlerter(maxThreshold, alerters);
-    statsAlerter.checkAndAlert([99.8, 34.2, 4.5, 6.7]);
-    console.log(emailAlert.emailSent, ledAlert.ledGlows);
-//     return stats;
+    stats.min = getMin(numbers);
+    stats.max = getMax(numbers);
+    stats.average = getAverage(numbers);
+    return stats;
 }
 
